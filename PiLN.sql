bb6220c2121a381205d5d10e98f4bb714f8cdc34
Lewis, Blake C <lewisbc@umsystem.edu>
2018-09-18 01:52:24 -0500

    afe wminor sql changes

--- PiLN.sql
+++ PiLN.sql
@@ -17,57 +17,58 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `PiLN`
+-- Database: PiLN
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Firing`
+-- Table structure for table Firing
 --
 
-CREATE TABLE IF NOT EXISTS `Firing` (
-  `run_id` int(11) NOT NULL DEFAULT '0',
-  `segment` int(11) NOT NULL DEFAULT '0',
-  `datetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `set_temp` decimal(8,2) NOT NULL,
-  `temp` decimal(8,2) NOT NULL,
-  `int_temp` decimal(8,2) DEFAULT NULL,
-  `pid_output` decimal(8,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS firing (
+  run_id     int(11) NOT NULL DEFAULT 0,
+  segment    int(11) NOT NULL DEFAULT 0,
+  dt         timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  set_temp   decimal(8,2) NOT NULL,
+  temp       decimal(8,2) NOT NULL,
+  int_temp   decimal(8,2) DEFAULT NULL,
+  pid_output decimal(8,2) NOT NULL,
+  PRIMARY KEY (run_id,segment,dt)
+);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Profiles`
+-- Table structure for table Profiles
 --
 
-CREATE TABLE IF NOT EXISTS `Profiles` (
-`run_id` int(11) NOT NULL,
-  `state` varchar(25) NOT NULL DEFAULT 'Staged',
-  `notes` text,
-  `p_param` float NOT NULL,
-  `i_param` float NOT NULL,
-  `d_param` float NOT NULL,
-  `start_time` timestamp NULL DEFAULT NULL,
-  `end_time` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+CREATE TABLE IF NOT EXISTS Profiles (
+  run_id   int(11) NOT NULL,
+  state    varchar(25) NOT NULL DEFAULT 'Staged',
+  notes text,
+  p_param  float NOT NULL,
+  i_param  float NOT NULL,
+  d_param  float NOT NULL,
+  start_time timestamp NULL DEFAULT NULL,
+  end_time   timestamp NULL DEFAULT NULL
+) AUTO_INCREMENT=34;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Segments`
+-- Table structure for table Segments
 --
 
-CREATE TABLE IF NOT EXISTS `Segments` (
-  `run_id` int(11) NOT NULL,
-  `segment` int(11) NOT NULL,
-  `set_temp` int(11) NOT NULL,
-  `rate` int(11) NOT NULL,
-  `hold_min` int(11) NOT NULL,
-  `int_sec` int(11) NOT NULL,
-  `start_time` timestamp NULL DEFAULT NULL,
-  `end_time` timestamp NULL DEFAULT NULL
+CREATE TABLE IF NOT EXISTS Segments (
+  run_id int(11) NOT NULL,
+  segment int(11) NOT NULL,
+  set_temp int(11) NOT NULL,
+  rate int(11) NOT NULL,
+  hold_min int(11) NOT NULL,
+  int_sec int(11) NOT NULL,
+  start_time timestamp NULL DEFAULT NULL,
+  end_time timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -75,32 +76,32 @@ CREATE TABLE IF NOT EXISTS `Segments` (
 --
 
 --
--- Indexes for table `Firing`
+-- Indexes for table Firing
 --
-ALTER TABLE `Firing`
- ADD PRIMARY KEY (`run_id`,`segment`,`datetime`);
+ALTER TABLE Firing
+ ADD PRIMARY KEY (run_id,segment,dt);
 
 --
--- Indexes for table `Profiles`
+-- Indexes for table Profiles
 --
-ALTER TABLE `Profiles`
- ADD PRIMARY KEY (`run_id`);
+ALTER TABLE Profiles
+ ADD PRIMARY KEY (run_id);
 
 --
--- Indexes for table `Segments`
+-- Indexes for table Segments
 --
-ALTER TABLE `Segments`
- ADD PRIMARY KEY (`run_id`,`segment`);
+ALTER TABLE Segments
+ ADD PRIMARY KEY (run_id,segment);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `Profiles`
+-- AUTO_INCREMENT for table Profiles
 --
-ALTER TABLE `Profiles`
-MODIFY `run_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+ALTER TABLE Profiles
+MODIFY run_id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
